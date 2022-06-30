2fd72325f78113ce0283d3e51481ee93f3a43366
Dependra Ramtel <dependraramtel@Dependras-MacBook-Pro.local>
2022-06-30 14:42:51 +0545

    Model created

--- BookPublication.API/Models/Book.cs
+++ BookPublication.API/Models/Book.cs
@@ -1,11 +1,34 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace BookPublication.API.Models
 {
 	public class Book
 	{
-		public int Id { get; set; }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]        
 		public string Title { get; set; }
-		public string ISBN { get; set; }
+        [Required]
+        public string ISBN { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public DateTime PublishedDate { get; set; }
+        [Required]
+        public string Edition { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        public string Category { get; set; }
+
+
+        public int PublicationId { get; set; }
+        public Publication Publication { get; set; }
+
+       
 
 	}
 }
---
+++ BookPublication.API/Models/Publication.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookPublication.API.Models
+{
+	public class Publication
+	{
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+		public string Name { get; set; }
+        [Required]
+        public string Country { get; set; }
+
+        [Required]
+        public List<Book> Books { get; set; }
+	}
+}
+
